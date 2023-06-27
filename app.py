a038750166638b0af13d0f7d168410267f719760
SoumiK-777 <soumik.kumar200803@gmail.com>
2023-06-28 00:26:18 +0530

    added app.py

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, redirect, url_for,session
+import tensorflow as tf
+import numpy as np
+import os
+import math
+import librosa
+from pydub import AudioSegment
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "ABCD@1234"
+
+DIR_NAME="./static/uploads"
+N_MFCC=13
+N_FFT=2048
+HOP_LENGTH=512
+SAMPLE_RATE = 22050
+TRACK_DURATION = 30 # measured in seconds
+SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
+NUM_SEGMENTS=5
+LABELS=["Bhajan","Bhojpuri","Bollywood Rap","Bollywood Romantic","Ghazal","Sufi"]
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    session['savepath'] = None
+    if request.method=="POST":
+        session['savepath'] = os.path.join(DIR_NAME, "temp.mp3")
+        request.files['music_file'].save(session['savepath'])
+        print(session['savepath'])
+        return redirect(url_for('predict'))
+    return render_template('index.html')
+
+@app.route('/predict')
+def predict():
+    input=[]
+    samples_per_segment = int(SAMPLES_PER_TRACK / NUM_SEGMENTS)
+    num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / HOP_LENGTH)
+    savepath = session['savepath']
+    model = tf.keras.models.load_model('cnn_model.h5')
+    output = os.path.join(DIR_NAME, "temp.wav")
+    sound = AudioSegment.from_mp3(savepath)
+    sound.export(output, format="wav")
+    signal, sample_rate = librosa.load(output, sr=SAMPLE_RATE, duration=TRACK_DURATION, offset=15)
+    for d in range(NUM_SEGMENTS):
+        start = samples_per_segment * d
+        finish = start + samples_per_segment
+        mfcc = librosa.feature.mfcc(y=signal[start:finish], sr=sample_rate, n_mfcc=N_MFCC, n_fft=N_FFT, hop_length=HOP_LENGTH)
+        mfcc = mfcc.T
+        if len(mfcc) == num_mfcc_vectors_per_segment:
+            input.append(mfcc.tolist())
+
+    prediction = model.predict(tf.expand_dims(input, axis=3))
+    prediction_index = prediction.argmax(axis=1)[0]
+    predicted_label = LABELS[prediction_index]
+
+    return predicted_label
+
+if __name__ == "__main__":
+    app.run(debug=True)
