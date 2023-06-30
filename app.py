0efd737c964e923622ecb9de70f92bb90b99ea70
SoumiK-777 <soumik.kumar200803@gmail.com>
2023-06-30 15:06:39 +0530

    updated app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ SAMPLE_RATE = 22050
 TRACK_DURATION = 60 # measured in seconds
 SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
 NUM_SEGMENTS=10
-LABELS=['Bhajan', 'Romantic', 'Bhojpuri', 'Rap']
+LABELS=['Bhajan', 'Bollywood Romantic', 'Bhojpuri', 'Bollywood Rap']
 
 model = tf.keras.models.load_model('cnn_model.h5')
 
@@ -27,33 +27,35 @@ def index():
     if request.method=="POST":
         session['savepath'] = os.path.join(DIR_NAME, "temp.mp3")
         request.files['music_file'].save(session['savepath'])
-        print(session['savepath'])
         return redirect(url_for('predict'))
     return render_template('index.html')
 
 @app.route('/predict')
 def predict():
-    input=[]
-    samples_per_segment = int(SAMPLES_PER_TRACK / NUM_SEGMENTS)
-    num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / HOP_LENGTH)
-    savepath = session['savepath']
-    output = os.path.join(DIR_NAME, "temp.wav")
-    sound = AudioSegment.from_mp3(savepath)
-    sound.export(output, format="wav")
-    signal, sample_rate = librosa.load(output, sr=SAMPLE_RATE, duration=TRACK_DURATION, offset=30)
-    for d in range(NUM_SEGMENTS):
-        start = samples_per_segment * d
-        finish = start + samples_per_segment
-        mfcc = librosa.feature.mfcc(y=signal[start:finish], sr=sample_rate, n_mfcc=N_MFCC, n_fft=N_FFT, hop_length=HOP_LENGTH)
-        mfcc = mfcc.T
-        if len(mfcc) == num_mfcc_vectors_per_segment:
-            input.append(mfcc.tolist())
+    try:
+        input=[]
+        samples_per_segment = int(SAMPLES_PER_TRACK / NUM_SEGMENTS)
+        num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / HOP_LENGTH)
+        savepath = session['savepath']
+        output = os.path.join(DIR_NAME, "temp.wav")
+        sound = AudioSegment.from_mp3(savepath)
+        sound.export(output, format="wav")
+        signal, sample_rate = librosa.load(output, sr=SAMPLE_RATE, duration=TRACK_DURATION, offset=30)
+        for d in range(NUM_SEGMENTS):
+            start = samples_per_segment * d
+            finish = start + samples_per_segment
+            mfcc = librosa.feature.mfcc(y=signal[start:finish], sr=sample_rate, n_mfcc=N_MFCC, n_fft=N_FFT, hop_length=HOP_LENGTH)
+            mfcc = mfcc.T
+            if len(mfcc) == num_mfcc_vectors_per_segment:
+                input.append(mfcc.tolist())
 
-    prediction = model.predict(tf.expand_dims(input, axis=3))
-    prediction_index = prediction.argmax(axis=1)[0]
-    predicted_label = LABELS[prediction_index]
+        prediction = model.predict(tf.expand_dims(input, axis=3))
+        prediction_index = prediction.argmax(axis=1)[0]
+        predicted_label = LABELS[prediction_index]
+    except:
+        return redirect(url_for('index'))
 
-    return predicted_label
+    return render_template('predict.html', predicted_label=predicted_label)
 
 if __name__ == "__main__":
     app.run(debug=True)
